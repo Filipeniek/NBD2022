329ecc06cd5c6a68cc11be15d79f6b6400a4b33b
Filipeniek <86184389+Filipeniek@users.noreply.github.com>
2022-06-30 15:02:01 +0200

    Add files via upload

---
+++ LAB_01/zapytanie_6.js
@@ -0,0 +1,3 @@
+printjson(db.people.insertOne({"sex":"Male", "first_name":"Stefan", "last_name":"Wichura","job":"Skoczek", "email":"s23911@pjwstk.edu.pl", "location":{"city":"Mielno", "address":{"streetname":"Kolorowa", "streetnumber":"2"}}, "description":"Skoczny Skoczek", "height":"172", "weight":"81","birth_date":"1987-07-06", "nationality":"Poland", "credit":{"type":"master card", "number":"1234123456785678","currency":"USD", "balance":"750"}}));
+
+
